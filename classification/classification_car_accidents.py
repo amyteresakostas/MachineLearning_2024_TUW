7384687ae0871e3ec189bcd5d2923acc3ccef367
KostasDamanakis <kon.damanakis@gmail.com>
2024-10-19 18:23:11 +0200

    update code for classification tasks

--- classification/classification_car_accidents.py
+++ classification/classification_car_accidents.py
@@ -10,9 +10,6 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-plt.rc('xtick', labelsize=8 )  # x tick labels fontsize
-plt.rc('ytick', labelsize=8 )  # y tick labels fontsize
-
 
 
 def plot_attributes(df):
@@ -24,61 +21,72 @@ def plot_attributes(df):
     os.makedirs(directory)
 
   list_with_interval_columns = ["Age_band_of_driver", "Driving_experience", "Service_year_of_vehicle", "Age_band_of_casualty"]
-  #columns which include ratio data, individual processing to order the intervals
-
-
-  fig, axis = plt.subplots(figsize = (10,6))
-  for col in df.columns:
+  dataframes  = {} # store copied dataframes, actually not necessary
+  ind = 0 # index for dataframes
   
+  for i in range(len(list_with_interval_columns)):
+      dataframes[f'df_copy_{i}'] = df.copy()
+
+
+  fig, axis = plt.subplots(figsize = (10,4))
+  for col in df.columns:
+ 
    if col=="Time":
    
     df_time = df.copy()
     df_time[col] = pd.to_datetime(df[col].astype(str), format='%H:%M:%S', errors='coerce')
 
-    plt.hist([t.hour + t.minute/60. for t in df_time[col]], bins = 24, color ="blue", edgecolor = "black", align='mid')
+    plt.hist([t.hour + t.minute/60. for t in df_time[col]], bins = 24, color='skyblue', edgecolor = "black", align='mid')
    
    elif col=="Day_of_week":
      #order strings based on weekdays
      days_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
      df[col] = pd.Categorical(df[col], categories=days_order, ordered=True)
-     sns.countplot(x  = col, data = df)
+
+     df[col].value_counts(sort = False).plot(kind='bar', color='skyblue', legend=False) #.reset_index()
+
    
    elif col in list_with_interval_columns:
-     
-     df_int = df.copy()
+       
      # create an additional column where you assign a number based on the interval and use this number to order the dataframe column
      # useful only for better visualization of the histogram
-     df_int["index"] = np.where(df_int[col].str.split('-').str.len()>1, df_int[col].str.split('-').str[0], 1000)
-     df_int["index"] = np.where((df_int[col].str.split('Under').str.len()>1) | (df_int[col].str.split('Below').str.len()>1), 0, df_int["index"])
-     df_int["index"] = np.where((df_int[col].str.split('Over').str.len()>1) | (df_int[col].str.split('Above').str.len()>1), 100, df_int["index"])
+     dataframes[f'df_copy_{ind}']["index"] = np.where(df[col].str.split('-').str.len()>1, df[col].str.split('-').str[0], 1000)
+     dataframes[f'df_copy_{ind}']["index"] = np.where((df[col].str.split('Under').str.len()>1) | (df[col].str.split('Below').str.len()>1), 0, dataframes[f'df_copy_{ind}']["index"])
+     dataframes[f'df_copy_{ind}']["index"] = np.where((df[col].str.split('Over').str.len()>1) | (df[col].str.split('Above').str.len()>1), 100, dataframes[f'df_copy_{ind}']["index"])
      
-     df_int["index"] = pd.to_numeric(df_int["index"])
-     df_int = df_int.sort_values(by='index').drop(columns='index').reset_index(drop=True)
-  
+     dataframes[f'df_copy_{ind}']["index"] = pd.to_numeric(dataframes[f'df_copy_{ind}']["index"])
+   
+     dataframes[f'df_copy_{ind}'] =  dataframes[f'df_copy_{ind}'].sort_values(by='index').drop(columns='index').reset_index(drop=True)
      
-     sns.countplot(x = col, data = df_int)
+     dataframes[f'df_copy_{ind}'][col].value_counts(sort=False).reindex(dataframes[f'df_copy_{ind}'][col].unique()).plot(kind='bar', color='skyblue', legend=False)
 
+     
+     ind +=1
+ 
    
    else:
      # for rest of nominal or ordinal data
-     sns.countplot(x  = col, data = df)
-   
+    
+     df[col].value_counts().plot(kind='bar', color='skyblue', legend=False)
+     
 
-   plt.ylabel('Occurence', fontsize = 10)
-   plt.xlabel(col, fontsize = 10)
+
+   plt.xlabel(col)
+   plt.ylabel("Frequency")
+
 
    if len(df[col].unique())>5 and df[col].apply(pd.to_numeric, errors='coerce').isnull().values.any() :
    #rotate xticks by 90 if column features more than 5 categories and the data of the column is not numerical
     if col!="Time":
-       plt.xticks(rotation=90)
- 
-   else:
-       plt.xticks(rotation=0)
+       plt.xticks(rotation=90, ha="right")
        
-   plt.tight_layout()
-   
-   
-   plt.savefig(directory + os.sep + '{}.png'.format(col)) #save as .pdf if image resolution is not good enough for latex
+   else:
+       plt.xticks(rotation=45, ha="right")
+       
+
+   plt.title(col)
+  
+   plt.savefig(directory + os.sep + '{}.png'.format(col), bbox_inches='tight') #save as .pdf if image resolution is not good enough for latex
    plt.clf()
 
 
